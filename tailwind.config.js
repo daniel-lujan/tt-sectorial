48bfd3bba8a34457fad6eb7125ef361cd1f41ab6
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-21 16:58:56 -0500

    chore: :wrench: Setup Tailwind and ESLint

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      sans: ["Raleway", "sans-serif"],
+      enphasis: ["Titillium Web", "serif"],
+    },
+    colors: {
+      primary: "#d43947",
+      secondary: "#ef8c96",
+      accent: "#f34858",
+      background: {
+        DEFAULT: "#fcf6f6",
+        600: "#FAECED",
+        900: "#FAE1E3",
+      },
+      text: "#100707",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
