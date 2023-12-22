a464bc92686e7f4ca8d1208c392b67c01d1a0193
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-22 11:04:22 -0500

    feat: :sparkles: Add repo link to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SvgIconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
