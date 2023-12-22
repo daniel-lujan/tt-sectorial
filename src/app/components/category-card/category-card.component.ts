99a5d136b411a241654ffd8c80d2946d46ea9248
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-21 21:14:12 -0500

    feat: :sparkles: Add CategoryCard component

---
+++ src/app/components/category-card/category-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener, Input } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+
+@Component({
+  selector: 'app-category-card',
+  standalone: true,
+  imports: [SvgIconComponent],
+  templateUrl: './category-card.component.html',
+  styleUrl: './category-card.component.css',
+})
+export class CategoryCardComponent {
+  @Input() category: Category = {} as Category;
+  isHovered = false;
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.isHovered = true;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.isHovered = false;
+  }
+}
