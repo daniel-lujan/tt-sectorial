fce9ddbc771601110955037e47d05fd02bb7bb9a
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-21 23:16:07 -0500

    feat: :sparkles: Add button for adding category in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,19 @@ import { BlogService } from '../../services/blog.service';
 import { AsyncPipe } from '@angular/common';
 import { CategoryCardComponent } from '../../components/category-card/category-card.component';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AsyncPipe, CategoryCardComponent, RouterLink],
+  imports: [
+    AsyncPipe,
+    CategoryCardComponent,
+    RouterLink,
+    ButtonComponent,
+    SvgIconComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
