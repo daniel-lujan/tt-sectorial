3adad41cba30871954b5e096db27c712db0039ed
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-21 21:34:41 -0500

    feat: :sparkles: Add category page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'category/:id',
+    component: CategoryComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/category/category.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { BlogService } from '../../services/blog.service';
+import { ActivatedRoute } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component';
+import { ButtonComponent } from '../../components/button/button.component';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { CategoryCardComponent } from '../../components/category-card/category-card.component';
+
+@Component({
+  selector: 'app-category',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    BreadcrumbComponent,
+    CategoryCardComponent,
+    ButtonComponent,
+    SvgIconComponent,
+  ],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css',
+})
+export class CategoryComponent {
+  categories = inject(BlogService).getCategories();
+  id = '';
+  data = {} as Category;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+    });
+
+    this.categories.result$.subscribe((res) => {
+      this.data =
+        res.data?.find((item) => item._id === this.id) ?? ({} as Category);
+    });
+  }
+}
