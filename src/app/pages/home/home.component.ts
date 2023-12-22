97455069c0b9f7a9b2bed9a504170885aa3a26a2
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-21 21:15:02 -0500

    feat: :sparkles: Add categories fetching and display at home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BlogService } from '../../services/blog.service';
+import { AsyncPipe } from '@angular/common';
+import { CategoryCardComponent } from '../../components/category-card/category-card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CategoryCardComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  categories = inject(BlogService).getCategories();
 }
