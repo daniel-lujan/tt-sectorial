9f46c28077104fb8151c62d748464890a51192d2
daniel-lujan <daniel.lujan@udea.edu.co>
2023-12-21 23:16:34 -0500

    feat: :sparkles: Add status chip to category card

--- src/app/components/category-card/category-card.component.ts
+++ src/app/components/category-card/category-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, HostListener, Input } from '@angular/core';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { StatusChipComponent } from '../status-chip/status-chip.component';
 
 @Component({
   selector: 'app-category-card',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, StatusChipComponent],
   templateUrl: './category-card.component.html',
   styleUrl: './category-card.component.css',
 })
